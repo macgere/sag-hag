ba0bff1134f7b2eedc90d6b4801f04ca701be7ab
Marc G <mac_gere@yahoo.com>
2022-05-20 16:30:06 -0500

    spruced it up

--- src/Crew.js
+++ src/Crew.js
@@ -1,5 +1,8 @@
+// things to do: make 'over-budget' message work, make delete buttons work
+
 import React from 'react';
 import { useState } from 'react';
+import Call from './Call';
 
 const Crew = () => {
     const [tooLowMessage, setMessage] = useState("")
@@ -7,27 +10,49 @@ const Crew = () => {
     const [name, setName] = useState("")
     const [dayRate, setDayRate] = useState(0)
     const [crewId, setCrewId] = useState(1)
+    const [hours, setHours] = useState("")
+    const [callTime, setCallTime] = useState(new Date())
+    const [maxBudge, setMaxBudge] = useState(0)
+    const [currBudge, setCurrBudge] = useState("")
 
     let submit = () => {
         setMessage("")
-        if (dayRate <= 200) {
+        let hoursNum = parseInt(hours)
+        if (dayRate <= 199) {
             setMessage("Sorry, that Day-Rate is too Low!")
+        } else if (hoursNum > 12) {
+            setMessage("Sorry, that's too many hours for one day!")
+        } else if (maxBudge < currBudge) {
+            setMessage("Sorry, you're Over-Budget!")    
         } else {
             setCrewId(crewId + 1)
+            const endTime = new Date(callTime.getTime() + (parseInt(hours) * 3600000))
             setCrewEntry([...crewEntry, {
-                crewId, name, dayRate
-            }
+                crewId, name, hours, endTime, dayRate
+                }
             ])
         }
     }
 
     return (
         <>
+            <Call setCallTime={setCallTime} callTime={callTime} />
+            <h3>Max Budget:</h3>
+            <input onChange={setMaxBudge} className='maxBudget' id='maxBudget'></input>
+            <h3 className='total' id='total'>SAG Budget for this day: ${crewEntry.map((e) => (e.dayRate)).reduce((acc, curr) => {
+                return acc + curr
+            }, 0)}
+            </h3>
+            <h3 className='crewTotal' id='crewTotal'>Total Crew on Call: {crewId - 1}</h3>
             <div className='crewMember' id='crewMember'>
-                <h3>Crew Member</h3>
+                <h3>Crew Member: </h3>
                 <div className="nameContainer" id="nameContainer">
                     <p className="nameText">Name</p>
                     <input onChange={(e) => setName(e.target.value)} className="nameInput" id="nameInput"></input>
+                </div>
+                <div className="hourContainer" id="hourContainer">
+                    <p className="hourText">Hours</p>
+                    <input onChange={(e) => setHours(e.target.value)} className="hourInput" id="hourInput"></input>
                 </div>
                 <div className='rateContainer'>
                     <p className='rateText'>Day-Rate</p>
@@ -39,21 +64,24 @@ const Crew = () => {
             <div className='crewList' id='crewList'>
                 <table>
                     <tr>
-                        <th>Crew Member #</th>
+                        {/* <th>Crew Member #</th> */}
                         <th>Name</th>
+                        <th>Hours</th>
+                        <th>End Time</th>
                         <th>Day-Rate</th>
+                        <th>Remove?</th>
                     </tr>
-                        {crewEntry.map((e) => (
-                            <tr>
-                                <td>{e.crewId}</td>
-                                <td>{e.name}</td>
-                                <td>{e.dayRate}</td>
-                            </tr>
-                        ))}
+                    {crewEntry.map((e) => (
+                        <tr>
+                            {/* <td>{e.crewId}</td> */}
+                            <td>{e.name}</td>
+                            <td>{e.hours}</td>
+                            <td>{e.endTime.toLocaleTimeString()}</td>
+                            <td>{e.dayRate}</td>
+                            <td><button className='deleteBtn' id='deleteBtn'>X</button></td>
+                        </tr>
+                    ))}
                 </table>
-                <h3 className='total' id='total'>$:{crewEntry.map((e) => (e.dayRate)).reduce((acc, curr) => {
-                    return acc + curr
-                }, 0)} </h3>
             </div>
         </>
     )
